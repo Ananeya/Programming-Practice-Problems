3fd2935727ce61f621fb7fc42e09ad27f52ba62f
Ananeya <ananeyap@gmail.com>
2025-03-15 22:03:07 +0300

    setup problem 8

---
+++ 08. Mixed Juices/juices.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*
+
+Your friend Li Mei runs a juice bar where she sells delicious mixed fruit juices. You are a frequent customer in her shop and realized you could make your friend's life easier. You decide to use your coding skills to help Li Mei with her job.
+
+1. Determine how long it takes to mix a juice
+2. Replenish the line wedge supply
+3. Finish up the shift
+
+*/
+
+
